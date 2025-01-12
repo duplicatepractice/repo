a753b573d1d61c160cc338be9c22d8a906db4921
umamaheswari0312 <umamaheswari.attada@gmail.com>
2025-01-12 17:15:02 +0530

    Update hungrytxt.py added jira water

--- hungrytxt.py
+++ hungrytxt.py
@@ -4,5 +4,6 @@ if hungry in ["yes","Y","y","Yes"]:
     print("Eat idly")
     print("Eat sambar")
 else:
-    print(""drink some water")
+    print("drink some water")
+    print("drink jira water")
     print("Do your homework")
